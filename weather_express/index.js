33bf3893d64dd5d4ce489d6e5c1e512abf56c747
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2023-01-25 13:14:38 +0200

    Created form request responce with API data

--- weather_express/index.js
+++ weather_express/index.js
@@ -27,4 +27,21 @@ app.get('/', function (req, res) {
         })
 })
 
+app.post('/', function (req, res){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+})
+
 app.listen(3000)
